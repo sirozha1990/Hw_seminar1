0f5f53176dd4657b00e8331ce96f34c3fea17ec4
Сержио <sirozha16@mail.ru>
2022-11-20 19:19:42 +0300

    добавили решение 1  задачи четвёртого  семинара

---
+++ Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16*/
+Console.Write("Введите число A  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B  ");
+int b = Convert.ToInt32(Console.ReadLine());
+int stepen = a;
+
+for (int i = 1; i < b; i++)
+{
+stepen = stepen * a;
+}
+Console.Write("A в степени B равно:  " + stepen);
