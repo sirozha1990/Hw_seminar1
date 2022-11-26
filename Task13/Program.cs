e8deca52f448374d27d25d0fe0bd8795e73740b4
Сержио <sirozha16@mail.ru>
2022-11-26 18:00:27 +0300

    добавили решение 1  задачи пятого  семинара

---
+++ Task13/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2*/
+Console.Write("Введите размер массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+ArrRandomNumbers(numbers);
+PrintArray(numbers);
+int count = 0;
+
+for (int x = 0; x < numbers.Length; x++)
+if (numbers[x] % 2 == 0)
+count++;
+
+Console.WriteLine($"{numbers.Length} чисел в массиве, {count} из них чётные");
+void ArrRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
